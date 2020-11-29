8caf95220a3eb8b3700b4f63feccbfd91d772e11
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2020-11-29 23:55:24 +0900

    hotfix

--- user.js
+++ user.js
@@ -39,7 +39,7 @@ user_pref("gfx.direct2d.force-enabled", true);               // default: false
 user_pref("gfx.font_rendering.cleartype.always_use_for_content", true);    // default: false
 user_pref("gfx.font_rendering.cleartype_params.cleartype_level", 100);     // default: -1
 user_pref("gfx.font_rendering.cleartype_params.pixel_structure", -1);       // default: -1
-user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5;        // default: -1
+user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5);        // default: -1
 user_pref("gfx.font_rendering.directwrite.enabled", true);                 // default: true
 user_pref("gfx.use_text_smoothing_setting", true);                         // default: false 
 
